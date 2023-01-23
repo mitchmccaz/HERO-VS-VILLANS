4a2b892d328c8bc29f4224d55cf59652fea5cc69
mitchmccaz <116661190+mitchmccaz@users.noreply.github.com>
2023-01-23 13:03:05 -0500

    Add files via upload

--- HERO VS VILLANS/super_types/views.py
+++ HERO VS VILLANS/super_types/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Hero
+from super_types.models  import Villan
+from super_types import HeroSerializer
 
-# Create your views here.
+
+@api_view(["GET"])
+def hero_list(request):
+
+    Hero = Hero.objects.all()
+    
+    serializer = HeroSerializer(Hero, many = True)
+
+
+    return Response (serializer.data)
